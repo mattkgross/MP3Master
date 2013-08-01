7869e2d2939b4ee93a7e48ea09aba0bbba7a6e49
Matt Gross <mattkgross@gmail.com>
2013-08-01 17:33:11 -0600

    Initial Setup

---
+++ MP3Master/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace MP3Master
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(884, 513);
+            this.MaximizeBox = false;
+            this.Name = "Main_Form";
+            this.Text = "MP3 Master";
+            this.Load += new System.EventHandler(this.Main_Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
