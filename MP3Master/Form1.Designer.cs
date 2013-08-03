7e854db55ad2ef209114aaea8d7935ac5ef4afc5
Matt Gross <mattkgross@gmail.com>
2013-08-02 20:47:50 -0600

    exportDirectory Selection Added

--- MP3Master/Form1.Designer.cs
+++ MP3Master/Form1.Designer.cs
@@ -28,7 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.exportDirectoryButton = new System.Windows.Forms.Button();
+            this.exportDirectoryBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.exportDirectoryTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe WP", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Export Organized Music To...";
+            // 
+            // exportDirectoryButton
+            // 
+            this.exportDirectoryButton.Location = new System.Drawing.Point(195, 31);
+            this.exportDirectoryButton.Name = "exportDirectoryButton";
+            this.exportDirectoryButton.Size = new System.Drawing.Size(75, 23);
+            this.exportDirectoryButton.TabIndex = 1;
+            this.exportDirectoryButton.Text = "Browse...";
+            this.exportDirectoryButton.UseVisualStyleBackColor = true;
+            this.exportDirectoryButton.Click += new System.EventHandler(this.exportDirectoryButton_Click);
+            // 
+            // exportDirectoryTextBox
+            // 
+            this.exportDirectoryTextBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.exportDirectoryTextBox.Location = new System.Drawing.Point(16, 33);
+            this.exportDirectoryTextBox.Name = "exportDirectoryTextBox";
+            this.exportDirectoryTextBox.Size = new System.Drawing.Size(173, 20);
+            this.exportDirectoryTextBox.TabIndex = 2;
+            this.exportDirectoryTextBox.TextChanged += new System.EventHandler(this.exportDirectoryTextBox_TextChanged);
             // 
             // Main_Form
             // 
@@ -36,15 +69,24 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(884, 513);
+            this.Controls.Add(this.exportDirectoryTextBox);
+            this.Controls.Add(this.exportDirectoryButton);
+            this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "Main_Form";
             this.Text = "MP3 Master";
             this.Load += new System.EventHandler(this.Main_Form_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button exportDirectoryButton;
+        private System.Windows.Forms.FolderBrowserDialog exportDirectoryBrowser;
+        private System.Windows.Forms.TextBox exportDirectoryTextBox;
     }
 }
 
--- MP3Master/Form1.cs
+++ MP3Master/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace MP3Master
 {
     public partial class Main_Form : Form
     {
+        private static string exportDirectory;
+
         public Main_Form()
         {
             InitializeComponent();
@@ -19,7 +22,26 @@ namespace MP3Master
 
         private void Main_Form_Load(object sender, EventArgs e)
         {
+            exportDirectory = exportDirectoryBrowser.RootFolder.ToString();
+        }
 
+        private void Updater()
+        {
+            exportDirectory = exportDirectoryTextBox.Text;
+        }
+
+        private void exportDirectoryButton_Click(object sender, EventArgs e)
+        {
+            if (exportDirectoryBrowser.ShowDialog() == DialogResult.OK)
+            {
+                exportDirectoryTextBox.Text = exportDirectoryBrowser.SelectedPath;
+                Updater();
+            }
+        }
+
+        private void exportDirectoryTextBox_TextChanged(object sender, EventArgs e)
+        {
+            Updater();
         }
     }
 }
