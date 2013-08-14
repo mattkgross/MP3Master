c5ebf7e2d971acbc795bd5726f39c248373c4502
Matt Gross <mattkgross@gmail.com>
2013-08-13 23:06:50 -0600

    Update to Testing - Starting Cases

--- MP3Master/MP3UnitTest/BasicFunctionalities.cs
+++ MP3Master/MP3UnitTest/BasicFunctionalities.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MP3UnitTest
@@ -6,10 +7,20 @@ namespace MP3UnitTest
     [TestClass]
     public class BasicFunctionalities
     {
+        private string path;
+
         [TestInitialize]
         private void Initialization()
         {
             // TODO: Create a fake mp3 file w/ attributes.
+            path = Directory.GetCurrentDirectory() + "\\test.mp3";
+            File.Create(path);
+        }
+
+        [TestCleanup]
+        private void Cleanup()
+        {
+            File.Delete(path);
         }
 
         [TestMethod]
